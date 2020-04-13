7f9a37e85a49554f31321eec6e38ee5e3eecf65e
Iza Witkowska <isa.witkowska@gmail.com>
2020-04-13 16:11:22 +0200

    REmove active from articles

--- js/script.js
+++ js/script.js
@@ -8,11 +8,23 @@ const titleClickHandler = function(event){
 
   /* remove class 'active' from all article links  */
 
+//Pytanie do Kamila: activeLink nie jest nigdzie osobno zdefiniowane. Czy pojedynczy link zostaje zdefinionwany poprostu przez to, ze jest pojedynczym elementem ze zbioru activeLinks?
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
   /* add class 'active' to the clicked link */
 
-
+ 
   /* remove class 'active' from all articles */
 
+  const activeArticles = document.querySelectorAll('.posts .active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
@@ -23,5 +35,5 @@ const titleClickHandler = function(event){
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+    link.addEventListener('click', titleClickHandler);
 }
