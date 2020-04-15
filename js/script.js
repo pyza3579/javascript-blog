e3c7741783c857834770e08ca9c204e770e388d6
Iza Witkowska <isa.witkowska@gmail.com>
2020-04-16 00:38:05 +0200

    For each article

--- js/script.js
+++ js/script.js
@@ -61,16 +61,20 @@
     titleList.innerHTML = '';
     
       /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    for(let article of articles) {
+      console.log(article);
+    }
 
-      /* get the article id */
+        /* get the article id */
 
-      /* find the title element */
+        /* find the title element */
 
-      /* get the title from the title element */
+        /* get the title from the title element */
 
-      /* create HTML of the link */
+        /* create HTML of the link */
 
-      /* insert link into titleList */
+        /* insert link into titleList */
   }
 
  generateTitleLinks();
